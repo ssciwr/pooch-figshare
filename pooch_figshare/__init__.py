32362c64d2e7802d336ba3f667786086e9abd6b3
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2026-03-23 14:15:19 +0100

    Import FigshareRepository in __init__

--- pooch_figshare/__init__.py
+++ pooch_figshare/__init__.py
@@ -1,2 +1,4 @@
 # The version file is generated automatically by setuptools_scm
 from pooch_figshare._version import version as __version__  # noqa: F401
+
+from pooch_figshare.repository import FigshareRepository
